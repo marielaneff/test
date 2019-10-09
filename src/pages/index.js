62e5b3d6d270de757e51fbe33d251d97368fe6d0
Marie Neff <marieneff@MacBook-Pro-de-Marie.local>
2019-10-09 16:05:05 +0200

    Test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,46 @@
-import React from "react"
+import React, { Component } from "react"
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 
-export default () => <div>Hello world!</div>
+
+
+class IndexPage extends Component {
+    render(){
+        const allPosts = this.proprs.data.allWordpressPost.edges 
+        return(
+        <div>
+            {allPosts.map(({ node }) => (
+        <div key={node.slug}>
+            <h3>{node.title}</h3>
+            </div>
+        )
+             )}
+            </div>
+        )
+    }
+}
+
+export default IndexPage
+
+
+
+export const pageQuery = graphql`
+query IndexPageQuery {
+ 
+  allWordpressPost {
+    edges {
+      node {
+        id
+        slug
+        title
+        content
+        excerpt
+        date
+        modified
+      }
+    }
+  }
+}
+
+`
